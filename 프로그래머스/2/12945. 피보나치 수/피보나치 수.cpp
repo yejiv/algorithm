5dd4fe24581cb09d627b87ba164661b12094996c
yetak <131342196+yejiv@users.noreply.github.com>
2026-03-26 20:39:23 +0900

    [level 2] Title: 피보나치 수, Time: 0.62 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -12,8 +12,17 @@ void f(int num)
 }
 
 int solution(int n) {        
-    table.push_back(0);
-    table.push_back(1);
-    f(n);
-    return table[n] ;
+    //table.push_back(0);
+    //table.push_back(1);
+    //f(n);
+    //return table[n] ;
+    vector<int> table(n + 1);
+
+    table[0] = 0;
+    table[1] = 1;
+    
+    for(int i = 2; i < n + 1; ++i)
+        table[i] = (table[i - 2] + table[i - 1]) % 1234567;
+    
+    return (table[n]);
 }
