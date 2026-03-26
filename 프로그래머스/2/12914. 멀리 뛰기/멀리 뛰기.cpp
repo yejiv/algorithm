025b88bb8186b251f02909016bfd0fbef0a03a29
yetak <131342196+yejiv@users.noreply.github.com>
2026-03-26 20:48:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -7,8 +7,7 @@ long long solution(int n) {
     long long answer = 1;
     int memo[2];
     
-    memo[0] = 1;
-    memo[1] = 1;
+    memo[0] = memo[1] = 1;
     
     for(int i = 2; i < n + 1; ++i)
     {
