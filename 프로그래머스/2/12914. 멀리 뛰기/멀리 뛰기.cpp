4c7fba4472c296657e51180f98b367f0d24361fd
yetak <131342196+yejiv@users.noreply.github.com>
2026-03-26 20:45:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -4,14 +4,18 @@
 using namespace std;
 
 long long solution(int n) {
-    long long answer = 0;
-    vector<int> table(n + 1);
-
-    table[0] = 1;
-    table[1] = 1;
+    long long answer = 1;
+    int memo[2];
+    
+    memo[0] = 1;
+    memo[1] = 1;
     
     for(int i = 2; i < n + 1; ++i)
-        table[i] = (table[i - 2] + table[i - 1]) % 1234567;
+    {
+        answer = (memo[0] + memo[1])% 1234567;
+        memo[0] = memo[1];
+        memo[1] = answer;
+    }
     
-    return table[n];
+    return answer;
 }
