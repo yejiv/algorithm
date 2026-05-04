b8c75f1d2a02765e9325dd36778605ca2ae7c548
yetak <131342196+yejiv@users.noreply.github.com>
2026-05-05 02:13:25 +0900

    [level 3] Title: 부대복귀, Time: 186.58 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+    vector<int> answer;
+    vector<vector<int>> m(n + 1);
+    vector<int> v (n + 1 , -1);
+    queue<pair<int,int>> q;
+    
+    for(const auto & A : roads)
+    {
+        m[A[0]].push_back(A[1]);
+        m[A[1]].push_back(A[0]);
+    }
+    
+    q.push({destination, 0});
+    v[destination] = 0;
+    
+    while(q.size())
+    {
+        for(const auto & A : m[q.front().first])
+        {
+            if (v[A] == -1)
+            {
+                q.push({A, q.front().second + 1});
+                v[A] = q.front().second + 1;
+            }
+        }
+        q.pop();
+    }
+    
+    for(const auto & A : sources)
+        answer.push_back(v[A]);
+    
+    return answer;
+}
