1ca112f11b504fab0ff2ab6d7b70f6c868279328
yetak <131342196+yejiv@users.noreply.github.com>
2026-05-06 02:38:07 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+vector<string> answer;
+unordered_map<string, vector<pair<string, bool>>> table;
+
+bool f(string depart, int size)
+{
+    if (answer.size() == size) return true;
+
+    for(int i = 0; i < table[depart].size(); ++i)
+    {
+        if (true == table[depart][i].second) continue;
+        
+        table[depart][i].second = true;
+        answer.push_back(table[depart][i].first);
+        
+        if (true == f(table[depart][i].first, size)) return true;
+        
+        table[depart][i].second = false;
+        answer.pop_back();
+    }
+    
+    return false;
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+        
+    for(const auto & A : tickets)
+        table[A[0]].push_back({A[1], false});
+    
+    for(auto & A : table)
+        sort(A.second.begin(), A.second.end());
+    
+    answer.push_back("ICN");
+    f("ICN", tickets.size() + 1);
+    
+    return answer;
+}
