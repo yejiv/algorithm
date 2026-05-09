9d5a47991a7fbee890a3de680f4e28fb86ec7aa8
yetak <131342196+yejiv@users.noreply.github.com>
2026-05-09 18:05:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,38 +1,38 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-    priority_queue<pair<int, int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    bool v[100] = {};
     vector<pair<int, int>> table[100];
-    int curnode = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    int count = 0;
+    bool v[100] = {};
     
     for(const auto & A : costs)
     {
-        table[A[0]].push_back({A[2], A[1]});
-        table[A[1]].push_back({A[2], A[0]});
+        table[A[0]].push_back({A[1], A[2]});
+        table[A[1]].push_back({A[0], A[2]});
     }
     
-    pq.push({0, 0});
+    pq.push({0, costs[0][0]});
     
-    while(!pq.empty())
+    while(count < n)
     {
-        int cost = pq.top().first;
-        int node = pq.top().second;
+        pair<int,int> curnode = pq.top();
         pq.pop();
         
-        if (v[node] == true) continue;
+        if (v[curnode.second] == true) continue;
         
-        v[node] = true;
-        answer += cost;
+        answer += curnode.first;
+        v[curnode.second] = true;
+        ++count;
         
-        for(const auto & A : table[node])
+        for(const auto & A : table[curnode.second])
         {
-            if (!v[A.second])
-                pq.push({A.first, A.second});
+            if (!v[A.first]) pq.push({A.second, A.first});
         }
     }
     
