342ba85026c5e5c7a29b5a251e86eb22afeeb3e2
yetak <131342196+yejiv@users.noreply.github.com>
2026-05-09 15:14:48 +0900

    [level 2] Title: 하노이의 탑, Time: 15.24 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void f(vector<vector<int>>& answer, int n, int start, int target, int other)
 {
-    if (n == 0) return;
+    if(n == 0) return;
     f(answer, n - 1, start, other, target);
     answer.push_back({start, target});
     f(answer, n - 1, other, target, start);
@@ -13,7 +13,7 @@ void f(vector<vector<int>>& answer, int n, int start, int target, int other)
 
 vector<vector<int>> solution(int n) {
     vector<vector<int>> answer;
-    
+
     f(answer, n, 1, 3, 2);
     
     return answer;
