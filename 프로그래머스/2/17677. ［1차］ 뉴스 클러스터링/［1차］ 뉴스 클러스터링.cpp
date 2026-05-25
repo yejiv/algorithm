eda46e6e9b2378bbab5629df0abb967e29067ed5
yetak <131342196+yejiv@users.noreply.github.com>
2026-05-25 17:07:33 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.07 ms, Memory: 4.32 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,41 +1,33 @@
 #include <string>
 #include <unordered_map>
-#include <iostream>
 using namespace std;
 
 int solution(string str1, string str2) {
     int answer = 0;
     unordered_map<string, int> map1;
     unordered_map<string, int> map2;
-    unordered_map<string, int> U;
-    int A = 0;
-    int B = 0;
+    int total1 = 0, total2 = 0;
+    int A = 0, B = 0;
     
     for(int i = 0; i < str1.size() - 1; ++i)
     {
         if (!isalpha(str1[i]) || !isalpha(str1[i + 1])) continue;
         map1[{ (char)toupper(str1[i]), (char)toupper(str1[i+1]) }] ++;
+        ++ total1;
     }
     for(int i = 0; i < str2.size() - 1; ++i)
     {
         if (!isalpha(str2[i]) || !isalpha(str2[i + 1])) continue;
         map2[{ (char)toupper(str2[i]), (char)toupper(str2[i+1]) }] ++;
+        ++ total2;
     }
     
     for(const auto & K : map1)
-        U[K.first] = max(K.second, map2[K.first]);
-    for(const auto & K : map2)
-        U[K.first] = max(K.second, map1[K.first]);
-    for(const auto & K : U)
-        B += K.second;
+        if (map2.count(K.first))
+            A += min(K.second, map2[K.first]);
     
-    unordered_map<string, int> & s = (map1.size() > map2.size()) ? map2 : map1;
-    unordered_map<string, int> & d = (map1.size() > map2.size()) ? map1 : map2;
+    B = total1 + total2 - A;
     
-    for(const auto & K : s)
-        if (d[K.first] > 0)
-            A += min(K.second, d[K.first]);
-    
-    if (A == 0 && B == 0) return 65536;
+    if (B == 0) return 65536;
     return (A * 65536) / B;
 }
