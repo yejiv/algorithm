d39f63b4d4e966c5fa705b101b7309d4e8f32b42
yetak <131342196+yejiv@users.noreply.github.com>
2026-06-06 20:14:32 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,42 +1,40 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <stack>
 #include <algorithm>
 using namespace std;
 
-vector<string> answer;
-unordered_map<string, vector<pair<string, bool>>> table;
-
-bool f(string depart, int size)
-{
-    if (answer.size() == size) return true;
-
-    for(int i = 0; i < table[depart].size(); ++i)
-    {
-        if (table[depart][i].second) continue;
-        
-        table[depart][i].second = true;
-        answer.push_back(table[depart][i].first);
-        
-        if (f(table[depart][i].first, size)) return true;
-        
-        table[depart][i].second = false;
-        answer.pop_back();
-    }
-    
-    return false;
-}
-
 vector<string> solution(vector<vector<string>> tickets) {
-        
+    vector<string> answer;
+    unordered_map<string, vector<string>> table;
+    stack<string> s;
+    
     for(const auto & A : tickets)
-        table[A[0]].push_back({A[1], false});
+        table[A[0]].push_back(A[1]);
     
     for(auto & A : table)
-        sort(A.second.begin(), A.second.end());
+        sort(A.second.begin(), A.second.end(), greater<>());
     
-    answer.push_back("ICN");
-    f("ICN", tickets.size() + 1);
+    s.push("ICN");
+    
+    while(!s.empty())
+    {
+        string cur = s.top();
+        
+        if (table[cur].empty())
+        {
+            answer.push_back(cur);
+            s.pop();
+        }
+        else
+        {
+            s.push(table[cur].back());
+            table[cur].pop_back();
+        }
+    }
+    
+    reverse(answer.begin(), answer.end());
     
     return answer;
 }
