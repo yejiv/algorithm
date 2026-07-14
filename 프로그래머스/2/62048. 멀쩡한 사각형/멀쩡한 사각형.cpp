5b8057a822eaea3a78f73d617dc87a963e7e9ebe
yetak <131342196+yejiv@users.noreply.github.com>
2026-07-15 00:51:37 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -1,14 +1,12 @@
-#include <algorithm>
-
 using namespace std;
 
 long long gcd(long long a, long long b) {
     return b == 0 ? a : gcd(b, a % b);
 }
 
-long long solution(int w,int h) {
-    long long mod = gcd(w, h);
-    long long count = mod - 1;
+long long solution(int w, int h) {
+    long long W = w;
+    long long H = h;
     
-    return (long long)w * h - ((w - 1 - count) + (long long)h);
+    return (W * H) - (W + H - gcd(W, H));
 }
